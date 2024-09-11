67b764ef8e10aadfb285d6e4c542e3631ee56ff0
Vrushali Ranadive <76587114+Vrushali31@users.noreply.github.com>
2024-09-11 22:31:57 +0000

    Updated test.py

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-import sam
-
-def test1_do_smtg():
-    assert sam.do_smtg(5) == 3
-
-def test2_do_smtg():
-    assert sam.do_smtg(7) == 8
+import hw2_debugging
+def test1():
+    assert hw2_debugging.mergeSort([3,6,8,2,5,30,24,9])==[2,3,5,6,8,9,24,30]
