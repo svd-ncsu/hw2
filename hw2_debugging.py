2c05964385aa644b210d92f9f21c68c653cd3baf
Devanshu Shah <92639289+devanshu-777@users.noreply.github.com>
2024-09-11 01:20:16 +0000

    Updated files using autopep8

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,12 +1,14 @@
 import rand
 
+
 def mergeSort(arr):
     if (len(arr) == 1):
         return arr
 
-    half = len(arr)//2
+    half = len(arr) // 2
 
     return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+
 
 def recombine(leftArr, rightArr):
     leftIndex = 0
@@ -22,11 +24,12 @@ def recombine(leftArr, rightArr):
 
     for i in range(rightIndex, len(rightArr)):
         mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
+
     for i in range(leftIndex, len(leftArr)):
         mergeArr[leftIndex + rightIndex] = leftArr[i]
 
     return mergeArr
+
 
 arr = rand.random_array([None] * 20)
 arr_out = mergeSort(arr)
